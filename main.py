43f837adf1d8a5916e8d3ea21dbb6f56cc53a772
Prince XXIV <91701011+princevvs@users.noreply.github.com>
2021-10-22 10:40:12 +0000

    Added more to the code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 os.system("clear")
 
 class Board():
-    def__init__(self):
+    def __init__(self) :
       self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 
     def display(self):
@@ -12,5 +12,34 @@ class Board():
         print("--------------")
         print(" %s | %s | %s " %(self.cells[7], self.cells[8], self.cells[9]))
 
+    def update_cell(self, cell_no, player): 
+      self.cells[cell_no] = player
+
 board = Board()
 board.display()
+
+def print_header():
+    print("Welcome to TicTacToe\n")
+
+#Refresh the screen
+#Then print the header
+#Show Board
+def refresh_screen():
+    os.system("clear")
+
+    print_header()
+
+    Board.display()
+
+while True:
+   refresh_screen()
+#Get the X input
+    x_choice = int(raw_input("\nX") Please choose 1 - 9")
+
+# Update board
+ board.update_cell(x_choice , "X")
+
+
+# Start at 5:40
+
+

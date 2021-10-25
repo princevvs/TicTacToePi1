3350df61cfcecc7fdd46f07ca7b31301d79be667
Prince XXIV <91701011+princevvs@users.noreply.github.com>
2021-10-25 11:35:42 +0000

    Modifications made

--- main.py
+++ main.py
@@ -13,7 +13,43 @@ class Board():
         print(" %s | %s | %s " %(self.cells[7], self.cells[8], self.cells[9]))
 
     def update_cell(self, cell_no, player): 
-      self.cells[cell_no] = player
+      if self.cells[cell_no] == " ":
+        self.cells[cell_no] = player
+
+    def is_winner(self, player):
+      if self.cells[1]==player and self.cells[2]==player and self.cells[3]==player:
+          return True
+      if self.cells[4]==player and self.cells[5]==player and self.cells[6]==player:
+          return True
+      if self.cells[7]==player and self.cells[8]==player and self.cells[9]==player:
+          return True
+      if self.cells[1]==player and self.cells[4]==player and self.cells[7]==player:
+          return True
+      if self.cells[2]==player and self.cells[5]==player and self.cells[8]==player:
+          return True
+      if self.cells[3]==player and self.cells[6]==player and self.cells[9]==player:
+          return True
+      if self.cells[1]==player and self.cells[5]==player and self.cells[9]==player:
+          return True
+      if self.cells[7]==player and self.cells[5]==player and self.cells[3]==player:
+          return True
+
+      return False
+
+    def is_tie(self):
+        used_cells = 0
+        for cell in self.cells:
+            if cell != " ":
+                used_cells += 1
+        if used_cells == 9:
+            return True
+        else:
+            return False
+
+
+    def reset(self):
+      self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 
 board = Board()
 board.display()
@@ -32,14 +68,62 @@ def refresh_screen():
     Board.display()
 
 while True:
-   refresh_screen()
+  refresh_screen()
+
 #Get the X input
-    x_choice = int(raw_input("\nX") Please choose 1 - 9")
+    x_choice = int(raw_input("\nX") Please choose 1 - 9"))
 
 # Update board
- board.update_cell(x_choice , "X")
+    board.update_cell(x_choice , "X")
+
+#Refresh screen function
+    refresh_screen()
+
+# Check for X winner
+    if board.is_winner("X"):
+        print("\nX wins!\n")
+        play_again = raw_input("Would you like to play again? (Y/N) > ").upper()
+        if play_again == "Y":
+            board.reset()
+            continue
+        else:
+            break
+
+# Check for tie game
+    if board.is_tie():
+        print("\nTie Game!\n")
+        play_again = raw_input("Would you like to play again? (Y/N) > ").upper()
+        if play_again == "Y":
+            board.reset()
+            continue
+        else:
+            break
 
 
-# Start at 5:40
+#Get the 0 input
+    o_choice = int(raw_input("\nO") Please choose 1 - 9")
 
+# Update board
+    board.update_cell(o_choice , "O")
 
+# Check for O winner
+    if board.is_winner("O"):
+        print("\nO wins!\n")
+        play_again = raw_input("Would you like to play again? (Y/N) > ").upper()
+        if play_again == "Y":
+            board.reset()
+            continue
+        else:
+            break
+
+# Check for tie game
+    if board.is_tie():
+        print("\nTie Game!\n")
+        play_again = raw_input("Would you like to play again? (Y/N) > ").upper()
+        if play_again == "Y":
+            board.reset()
+            continue
+        else:
+            break
+
+#PArt 5
